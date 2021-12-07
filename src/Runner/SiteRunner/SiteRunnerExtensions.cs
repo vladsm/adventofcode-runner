9f090af6710b0f8b3244dcf5bd1c149d66b173c8
Vladimir Smirnov <vladsm@gmail.com>
2021-12-08 00:05:02 +0300

    Remove the extesion methods that should not be in the core API

--- src/Runner/SiteRunner/SiteRunnerExtensions.cs
+++ src/Runner/SiteRunner/SiteRunnerExtensions.cs
@@ -12,43 +12,6 @@ public static class SiteRunnerExtensions
 		return runner.Puzzle(year, day, 2);
 	}
 
-	public static SiteRunner.IResultCorrectnessHandlerBuilder<TEntry, TResult> SolveFirstLevel<TEntry, TResult>(
-		this SiteRunner runner,
-		int year,
-		int day
-		)
-	{
-		return runner.Solve<TEntry, TResult>(year, day, 1);
-	}
-
-	public static SiteRunner.IResultCorrectnessHandlerBuilder<TEntry, TResult> SolveSecondLevel<TEntry, TResult>(
-		this SiteRunner runner,
-		int year,
-		int day
-		)
-	{
-		return runner.Solve<TEntry, TResult>(year, day, 2);
-	}
-
-	public static SiteRunner.IResultCorrectnessHandlerBuilder<TEntry, TResult> Solve<TEntry, TResult>(
-		this SiteRunner runner,
-		int year,
-		int day,
-		int level
-		)
-	{
-		string solverTypeName = $"AdventOfCode.Year{year}.Solvers.Day{day:00}Level{level}Solver, AdventOfCode2021.Solvers, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
-		Type? solverType = Type.GetType(solverTypeName, false);
-		if (solverType is null)
-		{
-			throw new InvalidOperationException(
-				$"Can not find solver type {solverTypeName} for the puzzle {year} day {day} level {level}"
-				);
-		}
-
-		return runner.Puzzle(year, day, level).SolveUsing<TEntry, TResult>(solverType);
-	}
-
 	public static SiteRunner.IResultCorrectnessHandlerBuilder<TEntry, TResult> SolveUsing<TEntry, TResult>(
 		this SiteRunner.ISiteRunnerBuilder builder,
 		Type solverType
